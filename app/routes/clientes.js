18518e3501e9ff3158a7a93c0df682a61d693742
Rodrigo França <rfrancacosta@gmail.com>
2018-08-05 13:59:00 -0300

    wip : estudando modules com conexao db

--- app/routes/clientes.js
+++ app/routes/clientes.js
@@ -1,12 +1,19 @@
 const connection = require('../../sql/connection');
+console.log(connection);
+
+
 connection();
-module.exports = function (app, connection) {    
-    function execSQLQuery(sqlQry, res) {
-        global.conn.request()
-            .query(sqlQry)
-            .then(result => res.json(result.recordset))
-            .catch(err => res.json(err));
-    }
+console.log(connection);
+
+
+function execSQLQuery(sqlQry, res) {
+    global.conn.request()
+        .query(sqlQry)
+        .then(result => res.json(result.recordset))
+        .catch(err => res.json(err));
+}
+
+module.exports = function (app) {
     app.get('/clientes', (req, res) => {
         execSQLQuery('SELECT * FROM Clientes', res);
     })
